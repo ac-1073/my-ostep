c44b57e6b3e920cd8fefee5e5de72e450b7e94ac
Andres Chamorro <ac1073@kill.cs.rutgers.edu>
2020-07-14 21:29:15 -0400

    wcat and wgrep complete

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -1,35 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv){
 
     if(argc == 1){
-        printf("wgrep: searchterm [file...]\n");
+        printf("wgrep: searchterm [file ...]\n");
         exit(1);
     }
 
+    size_t i;
+    char* term = *(++argv);
+    size_t term_len = strlen(term);
+
     if(argc == 2){
-        char buffer[100];
-        fgets(buffer, 100, stdin);
+
+        char buffer[250];
+        char* ptr = NULL;
+
+        while(fgets(buffer, sizeof(buffer), stdin) != NULL){
+
+            for(i = 0; i < strlen(buffer); i++){
+
+                ptr = &buffer[i]; 
+
+                if(*ptr == *term && strncmp(&buffer[i], term, term_len) == 0){
+                    printf("%s", buffer);
+                    break;       
+                }
+            }
+        }
+    exit(0);
     }
 
     FILE* fp;
     char* line = NULL;
     size_t len = 0;
     ssize_t read;
-    int i;
-    int cmp_len = 0;
-
-    char* term = *(++argv);
-    int term_len = 0;
-    char* tp = (char*)term;
-    while(*tp != '\0'){
-        term_len++;
-        tp++;
-    }
+    int j;
     argv++;
 
-    for(i = 2; i < argc; i++, argv++){
+    for(j = 2; j < argc; j++, argv++){
         fp = fopen(*argv, "r");
         if (fp == NULL){
             printf("wgrep: cannot open file\n");
@@ -37,26 +48,15 @@ int main(int argc, char** argv){
         }
 
         while((read = getline(&line, &len, fp)) != -1){
-            char* p1 = (char*)line;
-            char* p2 = (char*)term;
-            cmp_len = 0;
+            char* ptr = (char*)line;
 
-            while(*p1 != '\n'){
+            for(i = 0; i < len && *ptr != '\n' ; i++, ptr++){
 
-                if(*p1 == *p2){
-                    while(*p2 != '\0' && *p1 == *p2){
-                        cmp_len++;
-                        p2++;
-                        p1++;
-                    }
-                    if(cmp_len == term_len){
-                        printf("%s", line);
-                        break;
-                    }
-                    cmp_len = 0;
-                    p2 = (char*)term;
+                if(*ptr == *term && strncmp(ptr, term, term_len) == 0){
+                    printf("%s", line);
+                    break;
                 }
-                p1++;
+
             }
         }
         fclose(fp);
